000143f3a38ee7244729e05754e6029a44c88200
Nazar Voronych <voronych.nazik@gmail.com>
2018-12-19 12:43:43 +0200

    Updates

---
+++ precache-manifest.e791d92c3fd7cf863acf9990796c99be.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cf52e6c303fdad8f08d",
+    "url": "/resume/static/css/main.b7f3f2cc.chunk.css"
+  },
+  {
+    "revision": "3cf52e6c303fdad8f08d",
+    "url": "/resume/static/js/main.3cf52e6c.chunk.js"
+  },
+  {
+    "revision": "6e8d0942584e0bd54844",
+    "url": "/resume/static/js/1.6e8d0942.chunk.js"
+  },
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "7a4c35a8d792288323b75c01af2bb038",
+    "url": "/resume/static/media/Work-Portfolio.7a4c35a8.png"
+  },
+  {
+    "revision": "62dd5f04f212fbcd8ce7eeb8faaad96a",
+    "url": "/resume/static/media/nature-forest.62dd5f04.jpg"
+  },
+  {
+    "revision": "5fb50dccd63e7f5e2cfcbf7530f73e5e",
+    "url": "/resume/static/media/_1461.5fb50dcc.jpg"
+  },
+  {
+    "revision": "6e5c46f1aadf5c5a9e6fb3ec040f00fb",
+    "url": "/resume/static/media/WorkPortfolio2.6e5c46f1.png"
+  },
+  {
+    "revision": "4daea7b9fb43320efcc0671e596520b5",
+    "url": "/resume/static/media/_ionicons_svg_ios-person.4daea7b9.svg"
+  },
+  {
+    "revision": "78dd32829ce6bd94f1f09f6f6d04761d",
+    "url": "/resume/static/media/_ionicons_svg_md-list-box.78dd3282.svg"
+  },
+  {
+    "revision": "61f0abe937e6bed8dea8dbb45f5ef764",
+    "url": "/resume/static/media/_ionicons_svg_ios-brush.61f0abe9.svg"
+  },
+  {
+    "revision": "f5b9c225e4b22ef63ea0b6d140f19ec8",
+    "url": "/resume/static/media/_ionicons_svg_ios-at.f5b9c225.svg"
+  },
+  {
+    "revision": "8dc04bcddc1ee128393621e994aa53e9",
+    "url": "/resume/static/media/_ionicons_svg_logo-linkedin.8dc04bcd.svg"
+  },
+  {
+    "revision": "3ee3ca382ffca890ed37a4b96d876520",
+    "url": "/resume/static/media/_ionicons_svg_logo-twitter.3ee3ca38.svg"
+  },
+  {
+    "revision": "8a1ba32d3c7b69b57d05b74cd65d8bd8",
+    "url": "/resume/static/media/_ionicons_svg_logo-github.8a1ba32d.svg"
+  },
+  {
+    "revision": "46c0bd59adb9b6ca394698fd6169b290",
+    "url": "/resume/static/media/_ionicons_svg_logo-facebook.46c0bd59.svg"
+  },
+  {
+    "revision": "d4ae298a1525b3ca4c0ebf02857b9097",
+    "url": "/resume/static/media/_ionicons_svg_md-paper-plane.d4ae298a.svg"
+  },
+  {
+    "revision": "11442ba2b348e60bc193d04a39f61a18",
+    "url": "/resume/static/media/_ionicons_svg_ios-cloud-download.11442ba2.svg"
+  },
+  {
+    "revision": "91b10fb50ea37c7069ecf2fcdd93f5f0",
+    "url": "/resume/static/media/_ionicons_svg_md-chatboxes.91b10fb5.svg"
+  },
+  {
+    "revision": "3865f5d877792d8f8c723405ff451371",
+    "url": "/resume/static/media/coffee-outline.3865f5d8.svg"
+  },
+  {
+    "revision": "9436e0ada9a6349daaf672fd76cda124",
+    "url": "/resume/static/media/linux.9436e0ad.svg"
+  },
+  {
+    "revision": "8e22a2ec3ab442ba1eecb83a4433dc2b",
+    "url": "/resume/static/media/music.8e22a2ec.svg"
+  },
+  {
+    "revision": "24a01785cdeef5d5e549a83e6ede4532",
+    "url": "/resume/static/media/google-controller.24a01785.svg"
+  },
+  {
+    "revision": "016f7655de5f215cd1cc8741d7629c39",
+    "url": "/resume/static/media/briefcase.016f7655.svg"
+  },
+  {
+    "revision": "d49ad1bd12866e9b14c5f6be8bbcee5c",
+    "url": "/resume/static/media/school.d49ad1bd.svg"
+  },
+  {
+    "revision": "a2bcc2bef5576d61747d0a39b9e1491a",
+    "url": "/resume/static/media/skill.a2bcc2be.svg"
+  },
+  {
+    "revision": "c30c213863edfb05f2a4af85f778e994",
+    "url": "/resume/static/media/code-tags.c30c2138.svg"
+  },
+  {
+    "revision": "8e637650393a0dea59ae5ded2f596899",
+    "url": "/resume/static/media/arrow-right.8e637650.svg"
+  },
+  {
+    "revision": "1b172e03d416f6e2b86d36f170824ecb",
+    "url": "/resume/index.html"
+  }
+];
